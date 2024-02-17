7dab7cfb559127f14767d82b0905ae9dece9d88a
Aiswarya-98 <aiswaryasample789@gmail.com>
2024-02-17 11:23:05 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { Route, Router, Routes } from "react-router-dom"
 import "./App.css"
-import Banner from "./Components/Banner"
-import Nav from "./Components/Nav"
-import Row from "./Components/Row"
 import Home from "./Components/Home/Home"
 import Main from "./Components/Main/Main"
-import requests from "./requests"
 
 function App() {
   return (
